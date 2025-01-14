c5aead587e65b494ef092f3f08383838c60ec5e5
Timo <suca8817@gmail.com>
2025-01-14 15:32:25 +0900

    pagenation機能を停止

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import Homepage from './HomePage'
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { BlogPage } from './BlogPage'
-import { useEffect, useState } from 'react'
-import Album from './Type'
-import Pagenation from './Pagenation'
+// import { useEffect, useState } from 'react'
+// import Album from './Type'
+// import Pagenation from './Pagenation'
 import Three from './Three'
 
 
 function App() {
 
   //useStateのやつ
-  const [albums, setAlbums] = useState<Album[]>([]);
+  // const [albums, setAlbums] = useState<Album[]>([]);
 
-  //読み込み時に一回のみ発火されるやつ　jsonPlaceHolderからjsonをよみこんでconsolelogする
-  useEffect(() => {
-    const getDate = async () => {
-      await fetch("https://jsonplaceholder.typicode.com/albums/1/photos"
+  // //読み込み時に一回のみ発火されるやつ　jsonPlaceHolderからjsonをよみこんでconsolelogする
+  // useEffect(() => {
+  //   const getDate = async () => {
+  //     await fetch("https://jsonplaceholder.typicode.com/albums/1/photos"
 
-      ).then((res) => res.json() //受け取ったデータをresで受け取りres.jsonで返す
-    ).then((data) => {
-      setAlbums(data);
-      console.log(data);
-    });//res.jsonをdataで受け取りsetAlbumsに渡す
-    };
-    getDate();
-  }, [])
+  //     ).then((res) => res.json() //受け取ったデータをresで受け取りres.jsonで返す
+  //   ).then((data) => {
+  //     setAlbums(data);
+  //     console.log(data);
+  //   });//res.jsonをdataで受け取りsetAlbumsに渡す
+  //   };
+  //   getDate();
+  // }, [])
 
   //Main
   return (
